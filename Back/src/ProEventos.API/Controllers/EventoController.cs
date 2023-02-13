566b0e2828e7637b90860e71d71e48e1601d3147
Henrique Domanski <henrydomanski@gmail.com>
2023-02-13 08:44:01 +0800

    primeiro commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable <Evento> _evento = new Evento[]
+            {
+                new Evento ()
+                {
+
+                EventoId = 1,
+                Tema = "Angular e .NET 5",
+                Local = "São Paulo",
+                Lote = "Primeiro Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+                },
+
+                new Evento ()
+                {
+
+                EventoId = 2,
+                Tema = "Angular 15 e .NET 6",
+                Local = "Bahia",
+                Lote = "Segundo Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto2.png"
+                },
+
+            };
+
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}
